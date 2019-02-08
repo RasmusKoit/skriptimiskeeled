78ab9135b3d7bdb4a45485b36888c650483c96ec
Rasmus Koit <rasmuskoit@gmail.com>
2019-02-08 16:17:59 +0200

    Also finished symlink exc. Usage: ./symlink.sh <path>

---
+++ week1/symlinks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "No argument supplied"
+   	echo "Please specify path to search from"
+    	echo "for example: ./symlinks.sh /etc/nginx"
+else
+	find "$1" ! -readable -prune -o -type l -ls -print | grep " -> " | awk '{print $11 " " $12 " " $13}' > foundLinks.txt
+	echo "Search complete, results are in ./foundLinks.txt"
+fi
+
